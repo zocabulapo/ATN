53afe03b3550b84a9676b8eedd16335c09766c92
khang <105849087+zocabulapo@users.noreply.github.com>
2022-11-10 22:13:03 +0700

    Update content1.php

--- content1.php
+++ content1.php
@@ -29,7 +29,7 @@
                                 <i class="fa fa-phone"></i>
                             </div>
                             <div class="hero__search__phone__text">
-                                <h5>+84 90 785 3006</h5>
+                                <h5>0962010052</h5>
                                 <span>support 24/7 time</span>
                             </div>
                         </div>
